f409700f0c7272979516dbcb2d12bda8c235dfd9
Vicky Valerian <vickyvalerian27@gmail.com>
2023-08-15 21:18:30 +0700

    Manage Modification of Stock Report

--- resources/views/backend/pdf/stock_report_pdf.blade.php
+++ resources/views/backend/pdf/stock_report_pdf.blade.php
@@ -78,6 +78,10 @@
                                                             </td>
                                                             <td class="text-center"><strong>Product Name</strong>
                                                             </td>
+                                                            <td class="text-center"><strong>In Qty </strong>
+                                                            </td>
+                                                            <td class="text-center"><strong>Out Qty </strong>
+                                                            </td>
                                                             <td class="text-center"><strong>Stock </strong>
                                                             </td>
                                                         </tr>
@@ -85,6 +89,16 @@
                                                     <tbody>
                                                         <!-- foreach ($order->lineItems as $line) or some such thing here -->
                                                         @foreach ($allData as $key => $item)
+                                                            @php
+                                                                $buying_total = App\Models\Purchase::where('category_id', $item->category_id)
+                                                                    ->where('product_id', $item->id)
+                                                                    ->where('status', '1')
+                                                                    ->sum('buying_qty');
+                                                                $selling_total = App\Models\InvoiceDetail::where('category_id', $item->category_id)
+                                                                    ->where('product_id', $item->id)
+                                                                    ->where('status', '1')
+                                                                    ->sum('selling_qty');
+                                                            @endphp
                                                             <tr>
                                                                 <td class="text-center"> {{ $key + 1 }} </td>
                                                                 <td class="text-center"> {{ $item['supplier']['name'] }}
@@ -93,6 +107,8 @@
                                                                 <td class="text-center"> {{ $item['category']['name'] }}
                                                                 </td>
                                                                 <td class="text-center"> {{ $item->name }} </td>
+                                                                <td class="text-center"> {{ $buying_total }} </td>
+                                                                <td class="text-center"> {{ $selling_total }} </td>
                                                                 <td class="text-center"> {{ $item->quantity }} </td>
                                                             </tr>
                                                         @endforeach
